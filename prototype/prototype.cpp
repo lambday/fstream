adb91dceb4a79e927bec7d2b8923f666d06d2514
Soumyajit De <heavensdevil6909@gmail.com>
2017-04-01 18:24:11 +0530

    Update prototype.cpp

--- prototype/prototype.cpp
+++ prototype/prototype.cpp
@@ -83,6 +83,40 @@ class CBTestMMD
 		}
 		return normalize_statistic(result);
 	}
+
+	struct StatisticVarianceCollector
+	{
+		size_t num_terms;
+		float64_t statistic;
+		float64_t variance;
+		enum VarianceEstimationMethod;
+		StatisticVarianceCollector() : num_terms(1), statistic(0.0), variance(0.0) {}
+		static void add(StatisticVarianceCollector& c, const std::pair<float64_t,float64_t>& value)
+		{
+			auto delta = value.first - c.statistic;
+			c.statistic += delta/c.num_terms;
+			// variance computation logic..
+			c.num_terms++;
+		}
+	};
+
+	struct MultiKernelStatisticCollector
+	{
+		const SGVector<size_t> terms_counters;
+		const SGVector<float64_t>& mmds;
+		MultiKernelStatisticCollector(const SGVector<float64_t>& s, const SGVector<size_t>& tc)
+				: mmds(s), term_counters(tc)
+		{
+		}
+		static void add(MultiKernelStatisticCollector& c, std::pair<size_t,float64_t> value)
+		{
+			auto i = value.first;
+			auto delta = value.second - c.mmds[i];
+			c.mmds[i] += delta/c.terms_counters[i];
+			c.terms_counters[i]++;
+		}
+	};
+
 	/**
 	 * Example of packing multiple computation job per element in the stream and reducing to multiple values
 	 */
@@ -97,33 +131,17 @@ class CBTestMMD
 			return std::make_pair(statistic, variance);
 		};
 
-		struct MultiCollector
-		{
-			size_t num_terms;
-			float64_t statistic;
-			float64_t variance;
-			enum VarianceEstimationMethod;
-			MultiCollector() : num_terms(1), statistic(0.0), variance(0.0) {}
-			static void add(MultiCollector& mc, const std::pair<float64_t,float64_t>& value)
-			{
-				auto delta = value.first - mc.statistic;
-				mc.statistic += delta/mc.num_terms;
-				// variance computation logic..
-				mc.num_terms++;
-			}
-		};
-
-		MultiCollector mc;
+		StatisticVarianceCollector c;
 		if (kernel_matrix_precomputed)
 		{
 			auto collection = Stream::as_collection(kernel_matrix);
-			mc = collection.diagonal_block_stream(blocksize)
+			c = collection.diagonal_block_stream(blocksize)
 				.map(compute_jobs)
-				.collect([&mc]() { return mc; }, &MultiCollector::add);
+				.collect([&c]() { return c; }, &StatisticVarianceCollector::add);
 		}
 		else
 		{
-			mc = data_mgr.block_stream(blocksize)
+			c = data_mgr.block_stream(blocksize)
 				.map([this](const NextSamples& next_samples)
 				{
 					auto samples_p = next_samples.sample_at(0);
@@ -131,9 +149,23 @@ class CBTestMMD
 					return kernel->get_kernel_matrix(samples_p, samples_q);
 				})
 				.map(compute_jobs)
-				.collect([&mc]() { return mc; }, &MultiCollector::add);
+				.collect([&c]() { return c; }, &StatisticVarianceCollector::add);
 		}
-		return make_pair(normalize_statistic(mc.statistic), normalize_variance(mc.variance));
+		return make_pair(normalize_statistic(c.statistic), normalize_variance(c.variance));
+	}
+
+	static auto get_kernel_indices_by_distance_type(const KernelManager& kernel_mgr) const
+			-> std::unordered_map<DistanceType,std::vector<size_t>>
+	{
+		return IntStream::range(0, kernel_mgr.size())
+			.filter([&kernel_mgr](size_t i)
+			{
+				return kernel_mgr.kernel_at(i)->get_kernel_type() == SHIFT_INVARIANT;
+			})
+			.collect(Collectors::grouping_by([&kernel_mgr](size_t i)
+			{
+				return kernel_mgr.kernel_at(i)->get_distance()->get_distance_type();
+			});
 	}
 
 	/**
@@ -143,45 +175,66 @@ class CBTestMMD
 	{
 		SGVector<float64_t> mmds(kernel_mgr.size());
 		SGMatrix<float64_t,float64_t> Q(kernel_mgr.size(), kernel_mgr.size());
-		SGMatrix<float64_t,float64_t> cache(Q.num_rows, Q.num_cols);
 
-		auto categorized_kernel_inds = IntStream::range(0, kernel_mgr.size())
-			.filter([&kernel_mgr](size_t i)
-			{
-				return kernel_mgr.kernel_at(i)->get_kernel_type() == SHIFT_INVARIANT;
-			})
-			.collect(Collectors::grouping_by([&kernel_mgr](size_t i)
-			{
-				return kernel_mgr.kernel_at(i)->get_distance()->get_distance_type();
-			});
+		auto kernel_inds = get_kernel_indices_by_distance_type(kernel_mgr);
+		auto kernel_inds_c = Stream::as_collection(kernel_inds).stream()
+			.flat_map(IntStream::stream)
+			.compliment(IntStream::range(0, kernel_mgr.size()))
+			.collect(Collectors::to_stl_vector<size_t>());
 
-		// also, obtain the indices which are not shift invariant kernel
-		Stream::as_collection(categorized_kernel_inds).stream().flat_map() // TODO
+		SGVector<size_t> term_counters_statistic(kernel_mgr.size(), 1);
+		MultiKernelStatisticCollector c(mmds, term_counters_statistic);
 
 		data_mgr.block_stream(blocksize)
 			.map([&kernel_mgr,&cache](const NextSamples& next_samples)
 			{
 				auto samples_p = next_samples.sample_at(0);
 				auto samples_q = next_samples.sample_at(0);
+				auto merged = merge_samples(samples_p, samples_q);
 
-				Stream::as_collection(categorized_kernel_inds).stream()
-					.map([&samples_p,&samples_q,&kernel_mgr](const std::vector<size_t>& inds)
+				// compute mmds on kernels which can share distance instance
+				// we precompute the distance matrix for those kernels and compute the
+				// statistic on the fly.
+				c = Stream::as_collection(kernel_inds).stream()
+					.map([&merged,&kernel_mgr](const std::vector<size_t>& inds)
 					{
-						// there will always be at least one element in kernels, other_sample
-						// there won't be an entry in the grouby return map
-						auto dmatrix = kernel_mgr[inds[0]]->distance(samples_p, samples_q);
-						Stream::as_collection(inds).stream()
-							.map([&distance_matrix])(size_t i)
+						return std::make_pair(inds,kernel_mgr[inds[0]]->distance(merged, merged));
+					})
+					.map([](const std::pair<const std::vector<size_t>&,SGMatrix<float64_t>& inds_d)
+					{
+						return Stream::as_collection(inds_d.first).stream()
+							.map([&inds_d.second,&kernel_mgr](size_t i)
 							{
-								auto kernel = k->clone();
-								kernel->init(distance_matrix);
-								return clone->get_kernel_matrix();
-							}
-							.map(statistic_job)
-							.collect()
-					}
-					.map(statistic_job)
-					.collect();
-			}
+								auto kernel = kernel_mgr.kernel_at(i)->clone();
+								kernel->init(inds_d.second);
+								return std::make_pair(i,statistic_job(clone->get_kernel_matrix()));
+							})
+							.collect(Collectors::to_stl_vector<std::pair<size_t,float64_t>>());
+					})
+					.flat_map([](std::vector<std::pair<size_t,float64_t>> v)
+					{
+						return Stream::as_collection(v).stream();
+					})
+					.collect([&c]() { return c; }, &MultiKernelStatisticCollector::add);
+
+				// compute mmds on instances which do not share any characteristic with
+				// any which can be exploited to make the computation faster
+				c = Stream::as_collection(kernel_inds_c).stream()
+					.map([&merged,&kernel_mgr](const std::vector<size_t>& inds)
+					{
+						return Stream::as_collection(inds).stream()
+							.map([&kernel_mgr,&merged](size_t i)
+							{
+								auto kernel = kernel_mgr.kernel_at(i)->clone();
+								kernel->init(merged, merged);
+								return std::make_pair(i,statistic_job(clone->get_kernel_matrix()));
+							})
+							.collect(Collectors::to_stl_vector<std::pair<size_t,float64_t>>());
+					})
+					.collect([&c]() { return c; }, &MultiKernelStatisticCollector::add);
+
+					// we have the mmds for the current bunch, compute Q using usual logic
+			})
+			.collect(/* something */);
 	}
 }
