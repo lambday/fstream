b6acfa579ad751fb7df42799d3612460f46ccf7c
lambday <heavensdevil6909@gmail.com>
2017-04-06 18:46:51 +0530

    added lazy functor

--- src/shogun/lib/Functor.cpp
+++ src/shogun/lib/Functor.cpp
@@ -3,72 +3,140 @@
 #include <algorithm>
 #include <numeric>
 #include <iostream>
+#include <type_traits>
+#include <cmath>
 
 using std::declval;
 
-struct dummy
-{
-		template <class T> std::vector<T> operator()(T...) const;
-};
-
-template <class T, class... F>
-struct lazy_operator
-{
-		template <class U> lazy_operator<U, F...> operator()(U...) const;
-		lazy_operator() {}
-		lazy_operator(std::tuple<F...> _opstack) : opstack(_opstack) {}
-		std::tuple<F...> opstack;
-		// auto get(operand) // TODO
-};
-
-template <class F>
+template <template <class...> class Function>
 struct Functor
 {
+		template <class T>
+		using f_applied_type = decltype((declval<Function<T>>())(declval<T>()));
+
 		// fmap :: Functor f => (a -> b) -> f a -> f b
 		// takes a function that maps a -> b
 		// takes a f(a) type instance
 		// returns a f(b) type instance
 		template <class S, class T>
-		decltype(f(declval<T>())) fmap(std::function<T(S)>, decltype(f(declval<S>())));
-		F f;
-};
-
-struct EagerFunctor : Functor<dummy>
-{
-		using F = dummy;
-		template <class S, class T>
-		decltype(f(declval<T>())) fmap(std::function<T(S)> fn, decltype(f(declval<S>())) f_s)
+		static f_applied_type<T> fmap(const std::function<T(S)>& map, f_applied_type<S>&& f_s)
 		{
-				decltype(f(declval<T>())) _new(f_s.size());
-				std::transform(f_s.begin(), f_s.end(), _new.begin(), fn);
-				return _new;
+				return f_s.apply(map);
 		}
 };
 
-template <class L>
-struct LazyFunctor : Functor<lazy_operator<L>>
+template <class S>
+struct EagerFunctor : Functor<EagerFunctor>
 {
-		using F = lazy_operator<L>;
-		template <class S, class T>
-		decltype(f(declval<T>())) fmap(std::function<T(S)> fn, decltype(f(declval<S>())) f_s)
+		EagerFunctor<S> operator()(S...) const;
+
+		template <class T> using data_type = std::vector<T>;
+
+		EagerFunctor() : data(data_type<S>()) {}
+		EagerFunctor(const data_type<S>& _data) : data(_data)
 		{
-				decltype(f(declval<T>())) _new(std::tuple_cat(f_s.opstack, std::make_tuple(fn)));
-				return _new;
+				std::cout << "constructed from vector" << std::endl;
+		}
+
+		template <class T>
+		EagerFunctor<T> apply(const std::function<T(S)>& map) const
+		{
+				data_type<T> target(data.size());
+				std::transform(data.begin(), data.end(), target.begin(), map);
+				return EagerFunctor<T>(target);
+		}
+
+		const data_type<S>& get() const
+		{
+				return data;
+		}
+
+		const data_type<S>& data;
+};
+
+struct Vector
+{
+		template <class T> std::vector<T> operator()(T...) const;
+		template <class T, class S>
+		static std::vector<T> apply(const std::function<T(S)>& mapper, const std::vector<S>& source)
+		{
+				std::vector<T> target(source.size());
+				std::transform(source.begin(), source.end(), target.begin(), mapper);
+				return target;
 		}
 };
+
+template <class Function, class S, class T>
+struct LazyFunctor
+{
+		template <class A>
+		using f_applied_type = decltype((declval<Function>())(declval<A>()));
+
+		template <class Mapper>
+		LazyFunctor(const Mapper& map, const f_applied_type<S>& _f_s)
+		: mapper(map), f_s(_f_s)
+		{
+				std::cout << "constructed from vector" << std::endl;
+		}
+
+		template <class Mapper>
+		auto fmap(const Mapper& map) const
+		-> LazyFunctor<Function,S,decltype((declval<Mapper>())(declval<T>()))>
+		{
+				typedef decltype((declval<Mapper>())(declval<T>())) T_i;
+				return LazyFunctor<Function,S,T_i>([this, &map](const S& s)
+					{
+						return map(mapper(s));
+					}, f_s);
+		}
+
+		template <class T_i>
+		LazyFunctor<Function,S,T_i> fmap(T_i(* const map)(const T&)) const
+		{
+				return LazyFunctor<Function,S,T_i>([this, &map](const S& s)
+					{
+						return map(mapper(s));
+					}, f_s);
+		}
+
+		f_applied_type<T> get() const
+		{
+				return Function::apply(mapper, f_s);
+		}
+
+		const std::function<T(S)> mapper;
+		const f_applied_type<S>& f_s;
+};
+
+// make it a friend
+template <class S>
+LazyFunctor<Vector,S,S> make_functor(const std::vector<S>& f_s)
+{
+		return LazyFunctor<Vector,S,S>([](const S& s) { return s; }, f_s);
+}
+
+double sqrt(const int& a)
+{
+		return std::sqrt(a);
+}
 
 void test()
 {
 		std::vector<int> l(10);
-		std::iota(l.begin(), l.end(), 1);
-		std::function<std::string(int)> fn = [](int x) { return std::to_string(x); };
-		auto r = EagerFunctor().fmap(fn, l);
-		std::for_each(r.begin(), r.end(), [](const std::string& s)
+		std::iota(l.begin(), l.end(), 10);
+
+		auto r = make_functor(l)
+				.fmap(&sqrt)
+				.fmap([](double x)
 				{
-					std::cout << s << std::endl;
-				});
-		lazy_operator<int> lazy;
-		auto r2 = LazyFunctor<int>().fmap(fn, lazy);
+					return std::to_string(x);
+				})
+				.get();
+
+		std::for_each(r.begin(), r.end(), [](std::string s)
+		{
+			std::cout << s << std::endl;
+		});
 }
 
 int main()
